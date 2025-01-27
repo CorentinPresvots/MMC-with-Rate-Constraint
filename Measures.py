3ff79f13b2dd5c4db76451086b6efca944a0bcb5
CorentinPresvots <144250214+CorentinPresvots@users.noreply.github.com>
2025-01-27 19:46:08 +0100

    Measures.py

--- Measures.py
+++ Measures.py
@@ -260,11 +260,65 @@ def decode_variable_length(code):
     print("ind", ind)
     return ind
 
+
+import random
+
+from itertools import accumulate
+
+
+class Generation_Source:
+    
+    def __init__(self,alphabet=[],probability=[],verbose=False):
+        #inputs
+        self.alphabet=list(alphabet) # alphabet de la source
+        self.probability=list(probability)# probabilité d'apparition des symboles
+        self.verbose = verbose
+        
+        
+        #constants
+        self.size_alphabet=len(self.alphabet)
+        self.cumulate_probability=list(accumulate(self.probability)) # probabilités cumulé
+        
+        
+        #variables
+        self.probability_source=[0]*self.size_alphabet
+        self.source=[]
+  
+        
+        if (self.verbose):
+            print(f'alphabet: {self.alphabet}')
+            print(f'probability: {self.probability}')
+            print(f'cumulate probability: {self.cumulate_probability}')
+   
+    def reset(self):
+        
+        self.probability_source=[0]*self.size_alphabet
+        self.source=[]
+  
+    
+    def generate_one_symbole(self):
+        r = random.random()  # Générer un nombre aléatoire entre 0 et 1
+        for i in range(self.size_alphabet):
+            if r < self.cumulate_probability[i]:
+                return i
+
+                
+        
+    def generate_source(self,size_source):
+        self.source=[0]*size_source
+        for n in range(size_source):
+            i=self.generate_one_symbole()
+            
+            self.source[n]=self.alphabet[i]
+            self.probability_source[i]+=1
+        
+        for i in range(self.size_alphabet):
+            self.probability_source[i]/=size_source
+            
+        #self.cumulate_probability_real=list(accumulate(self.probability_real))
+         
         
 if __name__ == "__main__":
-    
-    # Import the source generation module
-    from Source import Generation_Source
     
     ### Generate a random source
     N = 20  # Length of the generated source
